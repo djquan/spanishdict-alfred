9f9f2100ab80d5bb304621c6f6b4c55a2f450199
Dan Quan <dan@quan.io>
2021-11-08 09:31:40 -0800

    Refactor into one script

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # SpanishDict Alfred Workflow
 
-## translate.rb
+main.rb currently provides two options, translation suggestions and conjugation suggestions
 
-translate.rb provides an interface for retrieving suggestions from spanishdict. These can be then fed into an Alfred action to open the resulting webpage.
+## :translation_suggestion
 
-## conjugate.rb
+this provides an interface for retrieving suggestions from spanishdict. These can be then fed into an Alfred action to open the resulting webpage.
 
-conjugate.rb is slightly smarter, and filters the results of the suggestions by if the resulting conjugation page exists.
+## :conjugation_suggestion
+
+this is slightly smarter, and filters the results of the suggestions by if the resulting conjugation page exists.
 
 As such, it is much slower than translate.rb as it's making web requests for every result (minus results with spaces in them as a rudimentary filter against sentence results).
+
+Note: There's a slight unexpected behavior where it will return english verbs, as SpanishDict's URL scheme is smart™ enough to translate the word before showing conjugation tables.
 
 ## caching
 
---
+++ src/main.rb
@@ -0,0 +1,68 @@
+require "net/http"
+require "cgi"
+require "json"
+require "uri"
+
+return unless ARGV[0] != nil
+
+CACHE_DIR = ENV["alfred_workflow_cache"] || "/tmp"
+CACHE_PREFIX = CACHE_DIR + "/spanishdict-alfred"
+Dir.mkdir(CACHE_DIR) unless Dir.exist?(CACHE_DIR)
+
+def make_cached_request(url)
+  cache_key = "#{CACHE_PREFIX}-#{CGI.escape(url)}"
+
+  if File.exist?(cache_key)
+    return File.read(cache_key)
+  end
+
+  response = Net::HTTP.get(URI.parse(url))
+  File.write(cache_key, response)
+  response
+end
+
+def get_suggestions(word)
+  url = "https://suggest1.spanishdict.com/dictionary/translate_es_suggest?q=#{word}&v=0"
+  response = make_cached_request(url)
+  results = JSON.parse(response)
+
+  results[:items] = results.delete("results").map do |v|
+    {
+      title: v,
+      arg: v,
+    }
+  end
+  results
+end
+
+def main(action)
+  results = get_suggestions(ARGV[0])
+  case action
+  when :translation_suggestion
+    unless results[:items].any? { |item| item[:title] == ARGV[0] }
+      results[:items] << {
+        title: ARGV[0],
+        arg: ARGV[0],
+      }
+    end
+  when :conjugation_suggestion
+    results[:items] = results[:items].select do |item|
+      next if item[:title].include?(" ")
+      query_url = "https://www.spanishdict.com/conjugate/#{item[:title]}"
+      response = make_cached_request(query_url)
+      !response.include?("Redirecting to")
+    end
+  end
+
+  results
+end
+
+puts main(:translation_suggestion).to_json
+
+if rand(5) == 3
+  Dir.glob(CACHE_PREFIX + "*").each do |x|
+    if File.stat(x).mtime < (Time.now - 604800)
+      File.delete(x)
+    end
+  end
+end
